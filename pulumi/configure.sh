20d36c5e5c94238d646d9ca59727b6d9ab295819
David Liu <david-khala@hotmail.com>
2023-02-03 15:44:26 +0800

    Update configure.sh

--- pulumi/configure.sh
+++ pulumi/configure.sh
@@ -2,7 +2,9 @@ org(){
   pulumi org set-default ${1:-davidkhala}
 
 }
-
+logback(){
+  pulumi logout && pulumi login
+}
 status() {
     # View backend, current stack, pending operations, and versions
     pulumi about
